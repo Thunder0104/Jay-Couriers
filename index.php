f6d5d34d1e71068461bf96fddc3a9588c1ddbfd9
Jayanth Gollapudi <72256512+Thunder0104@users.noreply.github.com>
2021-08-13 17:38:19 +0530

    Update index.php

--- index.php
+++ index.php
@@ -13,10 +13,7 @@
 
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
 <link rel="icon" href="favicon.ico" type="image/x-icon">
-<?php
-include "login.php";
-include "track-status.php";
-?>
+
 <style>
 .backdrop{
   content="";
